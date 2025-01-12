48ab1b3a2f1e284f2b3469718be7cf7e917d5ad9
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-11 21:00:37 -0300

    feat: :sparkles: add `Header` and `Layout` components

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,26 @@
-import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
+import { ThemeProvider } from "@emotion/react"
+import { Box, createTheme } from "@mui/material"
+import { Header } from "./app/components/Header"
+import { Layout } from "./app/components/Layout"
 
-const App = () => {
+const theme = createTheme({})
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Quotes />
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://reselect.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Reselect
-          </a>
-        </span>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        component="main"
+        sx={{
+          height: "100vh",
+          backgroundColor: "#000",
+        }}
+      >
+        <Header />
+        <Layout>
+          <h1>Hello World!</h1>
+        </Layout>
+      </Box>
+    </ThemeProvider>
   )
 }
 
