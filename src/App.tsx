b58b6a94912d7c7d07da0ed4b9c1dc16f0cc5d39
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2025-01-12 14:54:42 -0300

    feat: :sparkles: add example routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import { Box, Typography } from "@mui/material"
 import { Header } from "./components/Header"
 import { Layout } from "./components/Layout"
 import { appTheme } from "./config/theme"
+import { Route, Routes } from "react-router"
+
+const Home = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      Home
+    </Typography>
+  </Box>
+)
+
+const About = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      About
+    </Typography>
+  </Box>
+)
 
 function App() {
   return (
@@ -16,7 +33,10 @@ function App() {
       >
         <Header />
         <Layout>
-          <Typography variant="h1">Hello World!</Typography>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Box, Container } from "@mui/material"
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Box>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Container maxWidth="lg" sx={{ color: "white", mt: 4, mb: 4 }}>
         {children}
       </Container>
     </Box>
